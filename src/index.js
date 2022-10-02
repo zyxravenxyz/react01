ea668817f392f6d3bccb59bd021c37b7a02c12e1
Raven <zyxravenxyz@gmail.com.tw>
2022-10-02 21:02:11 +0800

    react1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import Game from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-
-  </React.StrictMode>
-);
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<h1> hello, world!</h1>);
+const name = "raven";
+const element1 = <h1>hello, {name}</h1>;
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+const user = {
+  firstName: "Raven",
+  lastName: "Li",
+};
+const element = <h1>Hello, {formatName(user)}</h1>;
 reportWebVitals();
