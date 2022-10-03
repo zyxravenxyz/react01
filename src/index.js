2239a6dcf4e7d2df3c0bc9346e3de1abf702eed5
Raven <zyxravenxyz@gmail.com.tw>
2022-10-04 03:07:05 +0800

    button

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ import "./index.css";
 // import Game from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1> hello, world!</h1>);
-const name = "raven";
-const element1 = <h1>hello, {name}</h1>;
-function formatName(user) {
-  return user.firstName + " " + user.lastName;
-}
-const user = {
-  firstName: "Raven",
-  lastName: "Li",
+const testFunction = () => {
+  return (
+    <div>
+      <button>大家好</button>
+      <h1> Hello world</h1>
+    </div>
+  );
 };
-const element = <h1>Hello, {formatName(user)}</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(testFunction());
+
 reportWebVitals();
